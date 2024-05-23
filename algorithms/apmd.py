bf9708577806e65f4184f308abc3a19c89b7252c
Mitssuki-Sakamoto <sakamoto_mitsuki@cyberagent.co.jp>
2024-05-23 18:19:08 +0900

    rename mutation_divergence to perturbation_divergence

--- algorithms/apmd.py
+++ algorithms/apmd.py
@@ -10,55 +10,55 @@ class APMD(MD):
         regularizer,
         learning_rate,
         mutation_rate,
-        mutation_divergence,
+        perturbation_divergence,
         update_slingshot_freq,
         **kwargs
     ):
         self.mutation_rate = mutation_rate
         self.slingshot_strategy = np.ones(num_actions) / num_actions
-        self.mutation_divergence = mutation_divergence
+        self.perturbation_divergence = perturbation_divergence
         self.update_slingshot_freq = update_slingshot_freq
         super().__init__(num_actions, regularizer, learning_rate, **kwargs)
 
     def name(self):
         alg_name = self.__class__.__name__
         if self.update_slingshot_freq is not None:
-            alg_name += "_utf{}".format(self.update_slingshot_freq)
+            alg_name += "_tsig{}".format(self.update_slingshot_freq)
         alg_name += "_mu{}".format(self.mutation_rate)
-        alg_name += "_div{}".format(self.mutation_divergence)
+        alg_name += "_div{}".format(self.perturbation_divergence)
         alg_name += "_lr{}".format(self.learning_rate)
         alg_name += "_{}".format(self.regularizer)
         return alg_name
 
     def add_gradient(self, gradient):
-        if self.mutation_divergence == "reverse_kl":
+        if self.perturbation_divergence == "reverse_kl":
             mutation = (
                 self.mutation_rate
                 * (self.slingshot_strategy - self.strategy)
                 / self.strategy
             )
-        elif self.mutation_divergence == "kl":
+        elif self.perturbation_divergence == "kl":
             mutation = -self.mutation_rate * (
                 np.log(self.strategy / self.slingshot_strategy) + 1
             )
-        elif self.mutation_divergence == "l2":
+        elif self.perturbation_divergence == "l2":
             mutation = -self.mutation_rate * (self.strategy - self.slingshot_strategy)
-        elif self.mutation_divergence == "chi":
+        elif self.perturbation_divergence == "chi":
             mutation = (
                 -self.mutation_rate
                 * 2
                 * (self.strategy - self.slingshot_strategy)
                 / self.slingshot_strategy
             )
-        elif self.mutation_divergence == "hellinger":
+        elif self.perturbation_divergence == "hellinger":
             mutation = -self.mutation_rate * (
                 1 - np.sqrt(self.slingshot_strategy / self.strategy)
             )
-        elif self.mutation_divergence == "js":
+        elif self.perturbation_divergence == "js":
             mutation = -self.mutation_rate * np.log(
                 2 * self.strategy / (self.strategy + self.slingshot_strategy)
             )
-        elif self.mutation_divergence == "sym_kl":
+        elif self.perturbation_divergence == "sym_kl":
             mutation = -self.mutation_rate * (
                 np.log(self.strategy / self.slingshot_strategy)
                 + 1
